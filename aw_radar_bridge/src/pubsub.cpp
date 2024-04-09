0f5fd4feddfe48d925debf33b7b5a1bd56a97220
Hector Cruz <hcruzgo@ed.ac.uk>
2024-04-09 11:32:14 +0100

    Refactor pubsub.cpp for improved readability.
    - Simplyfyin ROS parameter declaration
    - Using range-based for loop instead of iterator loop to reduce verbosity
    - Ensuring radar detection a_azang_hyp vector is non-empty
    - Adding doxygen-style comments

--- aw_radar_bridge/src/pubsub.cpp
+++ aw_radar_bridge/src/pubsub.cpp
@@ -11,55 +11,87 @@ using namespace ecal_to_ros::msg;
 using namespace radar_msgs::msg;
 using std::placeholders::_1;
 
-class PubSub : public rclcpp::Node
+/**
+ * @class RadarBridge
+ * @brief Converts continental's radar detection msgs into ROS perception radar scans msgs
+ *
+ * This class subscribes to radar detection, processes them into a standardized
+ * radar scan format, and publishes the results. It's designed to be used in systems
+ * where radar data needs to be transformed before further processing.
+ *
+ */
+class RadarBridge : public rclcpp::Node
 {
-  public:
-    PubSub()
-    : Node("aw_radar_pubsub")
+public:
+  RadarBridge()
+      : Node("aw_radar_pubsub")
+  {
+    /// Get frame id
+    frame_id_ = this->declare_parameter<std::string>("frame_id", "frame_name");
+
+    /// Ger radar input topic
+    input_topic_ = this->declare_parameter<std::string>("input_topic", "radar_input");
+
+    /// Define subscription for original Radar msgs
+    subscription_ = this->create_subscription<RadarDetectionImage>(
+        input_topic_, 10, std::bind(&RadarBridge::radar_CB, this, _1));
+
+    /// Get radar output topic
+    output_topic_ = this->declare_parameter("output_topic", "radar_output");
+
+    // Defin publisher for converted Radar msgs
+    publisher_ = this->create_publisher<RadarScan>(output_topic_, 10);
+  }
+
+private:
+
+  /**
+   * @brief Processes incoming radar detection images and publishes radar scans.
+   *
+   * @param conti_msg The received radar detection image message.
+   *
+   */
+  void radar_CB(const RadarDetectionImage &conti_msg) const
+  {
+    auto radar_scan_msg = RadarScan();
+    radar_scan_msg.header = conti_msg.header;
+    radar_scan_msg.header.frame_id = frame_id_;
+
+    // Convert continental radar msg into ROS perception radar_msgs struct
+    for (const auto &detection : conti_msg.a_radardetectionlist)
     {
-      this->declare_parameter("frame_id", rclcpp::PARAMETER_STRING);
-      this->get_parameter<std::string>("frame_id", frame_id_);
+      auto radar_return = RadarReturn();
+      radar_return.range = detection.f_range;              // Radial distance [m]
+      // Ensure detection azang hyp is non-empty
+      if (detection.a_azang_hyp.size() > 0)
+        radar_return.azimuth = detection.a_azang_hyp[0];   // Most likely hyp. [rad]
+      radar_return.elevation = 0.0f;                       // 2D flat radar space
+      radar_return.doppler_velocity = detection.f_vrelrad; // Rel speed [m/s]
+      radar_return.amplitude = 1;                          // Not provided by Conti [dB]
 
-      this->declare_parameter("input_topic", rclcpp::PARAMETER_STRING);
-      this->get_parameter<std::string>("input_topic", input_topic_);
-      subscription_ = this->create_subscription<RadarDetectionImage>(
-        input_topic_, 10, std::bind(&PubSub::topic_CB, this, _1));
-
-      this->declare_parameter("output_topic", rclcpp::PARAMETER_STRING);
-      this->get_parameter<std::string>("output_topic", output_topic_);
-      publisher_ = this->create_publisher<RadarScan>(output_topic_, 10);
+      radar_scan_msg.returns.push_back(radar_return);
     }
 
-  private:
-    void topic_CB(const RadarDetectionImage & conti_msg) const
-    {
-      auto radar_msg = RadarScan();
-      radar_msg.header = conti_msg.header;
-      radar_msg.header.frame_id = frame_id_;
+    publisher_->publish(radar_scan_msg);
+  }
 
-      for (std::vector<RadarDetection>::const_iterator
-        i = conti_msg.a_radardetectionlist.begin();
-        i != conti_msg.a_radardetectionlist.end(); ++i)
-      {
-        auto radar_return = RadarReturn();
-        radar_return.range = i->f_range;          // Radial distance [m]
-        radar_return.azimuth = i->a_azang_hyp[0]; // Most likely hyp. [rad]
-        radar_return.elevation = 0.0f;            // 2D flat radar space
-        radar_return.doppler_velocity = i->f_vrelrad;   // Rel speed [m/s]
-        radar_return.amplitude = 1;               // Not provided by Conti [dB]
-        radar_msg.returns.push_back(radar_return);
-      }
-      publisher_->publish(radar_msg);
-    }
-    std::string input_topic_, output_topic_, frame_id_;
-    rclcpp::Subscription<RadarDetectionImage>::SharedPtr subscription_;
-    rclcpp::Publisher<RadarScan>::SharedPtr publisher_;
+  /// Radar I/O and frame id str's
+  std::string input_topic_, output_topic_, frame_id_;
+
+  /// Subscritpion for Contintal's Radar msg
+  rclcpp::Subscription<RadarDetectionImage>::SharedPtr subscription_;
+
+  /// Publisher for ROS perception Radar msg
+  rclcpp::Publisher<RadarScan>::SharedPtr publisher_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<PubSub>());
+
+  rclcpp::spin(std::make_shared<RadarBridge>());
+
   rclcpp::shutdown();
+  
   return 0;
 }
