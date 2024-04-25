1e085d1055a14ea13a5330d54046ad8fda142e04
Alejandro Bordallo <alex.bordallo@ed.ac.uk>
2024-04-25 13:09:48 +0100

    Update pre-commit setup on init_config (#2)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM ros:humble-ros-base-jammy AS base
 
 # Install key dependencies
-RUN apt update \
+RUN apt-get update \
     && DEBIAN_FRONTEND=noninteractive \
-        apt -y --quiet --no-install-recommends install \
+        apt-get -y --quiet --no-install-recommends install \
         # Install ROS msg dependencies
-        ros-$ROS_DISTRO-radar-msgs \
-        ros-$ROS_DISTRO-sensor-msgs \
+        ros-"$ROS_DISTRO"-radar-msgs \
+        ros-"$ROS_DISTRO"-sensor-msgs \
     && rm -rf /var/lib/apt/lists/*
 
 # Setup ROS workspace folders
@@ -22,7 +22,7 @@ COPY ecal_to_ros/ros2/ src/ecal_to_ros/
 COPY aw_radar_bridge  src/aw_radar_bridge
 
 # Source ROS2 setup for dependencies and build our code
-RUN . /opt/ros/$ROS_DISTRO/setup.sh && \
+RUN . /opt/ros/"$ROS_DISTRO"/setup.sh && \
     colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 # -----------------------------------------------------------------------
@@ -30,9 +30,9 @@ RUN . /opt/ros/$ROS_DISTRO/setup.sh && \
 FROM base AS dev
 
 # Install basic dev tools (And clean apt cache afterwards)
-RUN apt update \
+RUN apt-get update \
     && DEBIAN_FRONTEND=noninteractive \
-        apt -y --quiet --no-install-recommends install \
+        apt-get -y --quiet --no-install-recommends install \
         # Command-line editor
         nano \
         # Ping network tools
@@ -41,11 +41,12 @@ RUN apt update \
         bash-completion \
     && rm -rf /var/lib/apt/lists/*
 
-# Add sourcing local workspace command to bashrc for convenience when running interactively
-RUN echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> /root/.bashrc
-
+# Add sourcing local workspace command to bashrc when running interactively
 # Add colcon build alias for convenience
-RUN echo 'alias colcon_build="colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release && source install/setup.bash"' >> /root/.bashrc
+RUN echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> /root/.bashrc && \
+    echo 'alias colcon_build="colcon build --symlink-install \
+            --cmake-args -DCMAKE_BUILD_TYPE=Release && \
+            source install/setup.bash"' >> /root/.bashrc
 
 # Enter bash for development
 CMD ["bash"]
@@ -56,9 +57,9 @@ FROM base AS runtime
 
 COPY --from=prebuilt $ROS_WS/install $ROS_WS/install
 
-# Add command to docker entrypoint to source newly compiled code when running docker container
+# Add command to docker entrypoint to source newly compiled code in container
 RUN sed --in-place --expression \
-      '$isource "$ROS_WS/install/setup.bash"' \
+      "\$isource \"$ROS_WS/install/setup.bash\" " \
       /ros_entrypoint.sh
 
 # Launch ros package
--- aw_radar_bridge/src/radar_bridge_node.cpp
+++ aw_radar_bridge/src/radar_bridge_node.cpp
@@ -7,87 +7,84 @@
 #include "radar_msgs/msg/radar_scan.hpp"
 
 using namespace std::chrono_literals;
-using namespace ecal_to_ros::msg;
-using namespace radar_msgs::msg;
+using ecal_to_ros::msg::RadarDetectionImage;
+using radar_msgs::msg::RadarReturn;
+using radar_msgs::msg::RadarScan;
 using std::placeholders::_1;
 
 namespace sensor
 {
 /**
  * @class RadarBridge
- * @brief Converts continental's radar detection msgs into 
+ * @brief Converts continental's radar detection msgs into
  *        ROS perception radar scans msgs
  *
  * This class subscribes to radar detection, processes them into a standardized
- * radar scan format, and publishes the results. It's designed to be used 
- * in systems where radar data needs to be transformed before 
+ * radar scan format, and publishes the results. It's designed to be used
+ * in systems where radar data needs to be transformed before
  * further processing.
  *
  */
 class RadarBridge : public rclcpp::Node
 {
 public:
-  RadarBridge(const rclcpp::NodeOptions & options)
-      : Node("radar_bridge_node", options)
+  explicit RadarBridge(const rclcpp::NodeOptions & options) : Node("radar_bridge_node", options)
   {
     /// Get frame id
-    frame_id_ = 
-      this->declare_parameter<std::string>("frame_id", "frame_name");
+    frame_id_ = this->declare_parameter<std::string>("frame_id", "frame_name");
 
     /// Ger radar input topic
-    input_topic_ = 
-      this->declare_parameter<std::string>("input_topic", "radar_input");
+    input_topic_ = this->declare_parameter<std::string>("input_topic", "radar_input");
 
     /// Define subscription for original Radar msgs
     subscription_ = this->create_subscription<RadarDetectionImage>(
-        input_topic_, 10, std::bind(&RadarBridge::radar_CB, this, _1));
+      input_topic_, 10, std::bind(&RadarBridge::radar_CB, this, _1));
 
     /// Get radar output topic
-    output_topic_ = 
-      this->declare_parameter("output_topic", "radar_output");
+    output_topic_ = this->declare_parameter("output_topic", "radar_output");
 
     // Define publisher for converted Radar msgs
-    publisher_ = 
-      this->create_publisher<RadarScan>(output_topic_, 10);
+    publisher_ = this->create_publisher<RadarScan>(output_topic_, 10);
   }
 
 private:
-
   /**
    * @brief Processes incoming radar detection images and publishes radar scans.
    *
    * @param conti_msg The received radar detection image message.
    *
    */
-  void radar_CB(const RadarDetectionImage &conti_msg) const
+  void radar_CB(const RadarDetectionImage & conti_msg) const
   {
     auto radar_scan_msg = RadarScan();
     radar_scan_msg.header = conti_msg.header;
     radar_scan_msg.header.frame_id = frame_id_;
 
     // Convert continental radar msg into ROS perception radar_msgs struct
-    for (const auto &detection : conti_msg.a_radardetectionlist)
+    for (const auto & detection : conti_msg.a_radardetectionlist)
     {
       auto radar_return = RadarReturn();
 
       // Radial distance [m]
-      radar_return.range = detection.f_range;        
+      radar_return.range = detection.f_range;
 
       // Ensure detection azang hyp is non-empty
       if (detection.a_azang_hyp.size() > 0)
+      {
         // Most likely hyp. [rad]
         radar_return.azimuth = detection.a_azang_hyp[0];
+      }
 
-      // 2D flat radar space   
+      // 2D flat radar space
       radar_return.elevation = 0.0f;
 
-      // Rel speed [m/s]                       
-      radar_return.doppler_velocity = detection.f_vrelrad; 
+      // Rel speed [m/s]
+      radar_return.doppler_velocity = detection.f_vrelrad;
 
       // Not provided by Conti [dB]
-      radar_return.amplitude = 1;                          
+      radar_return.amplitude = 1;
 
-      // Push radar return 
+      // Push radar return
       radar_scan_msg.returns.push_back(radar_return);
     }
 
@@ -103,7 +100,7 @@ private:
   /// Publisher for ROS perception Radar msg
   rclcpp::Publisher<RadarScan>::SharedPtr publisher_;
 };
-} // sensor namespace
+}  // namespace sensor
 
 #include <rclcpp_components/register_node_macro.hpp>
 RCLCPP_COMPONENTS_REGISTER_NODE(sensor::RadarBridge)
